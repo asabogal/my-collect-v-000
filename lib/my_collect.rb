7cfdf07a444a5bbd0aac6deaf400128fd5922cc1
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-10 16:30:26 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
-def my_collect(collection)
-  i = 0
-  while i < array.length
-    yield(array[i])
-    i += 1
-  end
-end
+# def my_collect(collection)
+#   i = 0
+#   while i < array.length
+#     yield(array[i])
+#     i += 1
+#   end
+# end
